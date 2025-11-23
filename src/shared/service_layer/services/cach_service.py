ed57a501ce40b1fafca3b53c91ca4ebcf58b237a
amrinooralishahi <amirnor2344@gmail.com>
2025-11-23 11:02:11 +0330

    start write cach_service

--- src/shared/service_layer/services/cach_service.py
+++ src/shared/service_layer/services/cach_service.py
@@ -1,12 +1,43 @@
-import abc
+from abc import ABC, abstractmethod
 import json
 from datetime import timedelta
 from typing import Optional
 import redis
 from redis import Redis
 from core import settings
-from core.excepions import  ValidationException
-from core.logger import logger
+from core.excepions import ValidationException
+from core.logger import Logger
 from core.translates import validation_trans
-from core.types import  RedisConfig
+from core.types import RedisConfig
+
+logger = Logger('chach-service')
+
+
+class CacheService(ABC):
+    def pop(self, key: str) -> str | None:
+        value = self.get(key)
+        if value:
+            self.delete(key)
+        return value
+
+    @abstractmethod
+    def get(self, key: str) -> str | None:
+        ...
+
+    @abstractmethod
+    def set(self, key: str, value: str, exp: timedelta) -> None:
+        ...
+
+    @abstractmethod
+    def delete(self, key: str) -> None:
+        ...
+
+    @abstractmethod
+    def set_object(self, key: str, value: dict, exp: timedelta | None = None) -> None: ...
+
+    @abstractmethod
+    def get_object(self, key: str) -> dict | None: ...
+
+class RedisCacheService(CacheService):
+    _client: Redis | None
 
