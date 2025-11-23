22aa8b16e5914bfbefca697f0ac38731b8591895
amrinooralishahi <amirnor2344@gmail.com>
2025-11-23 11:39:24 +0330

    start write code unit_of_work ;

---
+++ src/unit_of_work.py
@@ -0,0 +1,13 @@
+from abc import ABC ,abstractmethod
+
+from sqlalchemy import text
+from sqlalchemy.orm import Session
+from account.adapters import repositories as account_repos
+from advertisement.adapters ioprt repositories as ad_repos
+from contract.adapters import repositories as contract_repos
+from core.base.abstract_unit_of_work import AbstractUnitOfWork
+from crm.adapters import repositories as crm_repos
+from shared.service_layer.services.storage_servce import StorageService
+
+
+
