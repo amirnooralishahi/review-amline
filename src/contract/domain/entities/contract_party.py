1044931c5cec60784386f73908135ae5b5a3c5aa
unknown <amirnor2344@gmail.com>
2025-11-26 05:50:03 -0800

    end write contract_party file for domain contract service

---
+++ src/contract/domain/entities/contract_party.py
@@ -0,0 +1,62 @@
+import datetime as dt
+from account.domain.enums import UserRole
+from contract.domain.enums import PartyType, Signaturetype
+from core.base.base_entity import BaseEntity
+
+
+class ContractParty(BaseEntity):
+    id: int
+    contract_id: int
+    user_id: int
+    user_role: UserRole
+    party_type: PartyType
+
+    signature_type: Signaturetype | None
+    signature_at: dt.datetime | None
+    signature_data: dict | None
+
+    created_at: dt.datetime
+    updated_at: dt.datetime | None
+    deleted_at: dt.datetime | None
+
+    def __init__(
+        self,
+        contract_id: int,
+        user_id: int,
+        party_type: PartyType,
+        user_role: UserRole = UserRole.PERSON,
+        **kwargs
+    ) -> None:
+        self.id = self.next_id
+        self.contract_id = contract_id
+        self.user_id = user_id
+        self.user_role = user_role
+        self.party_type = party_type
+
+    def deleted(self) -> None:
+        self.deleted_at = dt.datetime.now(tz=dt.timezone.utc)
+
+    def dumps(self, **kwargs):
+        return dict(
+            id=str(self.id),
+            contract=kwargs.pop("contract", dict(id=str(self.contract_id))),
+            user=kwargs.pop("user", dict(id=str(self.user_id), role=self.user_role)),
+            party_type=PartyType.resolve(self.party_type),
+            signature_type=Signaturetype.safe_resolve(self.signature_type),
+            signed_at=self.signed_at,
+            **kwargs,
+        )
+
+    def sign(
+        self,
+        signature_data: dict,
+        signed_at: dt.datetime,
+        signature_type: Signaturetype = Signaturetype.OTP,
+    ) -> None:
+        self.signature_type = signature_type
+        self.signature_data = signature_data
+        self.signed_at = signed_at
+
+    @property
+    def has_been_signed(self) -> bool:
+        return self.signed_at is not None
