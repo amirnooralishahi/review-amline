85e52b8e6492352da6b6fbafb55513cada6d221e
unknown <amirnor2344@gmail.com>
2025-11-26 23:23:38 -0800

    start write code file for enitites for service shared

---
+++ src/contract/domain/entities/Contract.py
@@ -0,0 +1,7 @@
+import datetime as dt 
+import random 
+import string 
+from account.domain.entities.user import User 
+from contract.domain import enums 
+from shared.domain.entities.file import File
+ 
