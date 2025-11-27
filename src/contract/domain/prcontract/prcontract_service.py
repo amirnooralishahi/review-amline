d33cbccd5cf35d322587ce614c8474fce46977f6
unknown <amirnor2344@gmail.com>
2025-11-26 23:19:03 -0800

    start write code for property_rent_contract entities for contract service

--- src/contract/domain/prcontract/prcontract_service.py
+++ src/contract/domain/prcontract/prcontract_service.py
@@ -1,2 +1,9 @@
 from account.domain.entities.user import User 
 from contract.domain.entities.contract_party import ContractParty
+from contract.domain.entities.contract_step import ContractStep 
+from contract.domain.entities.property_rent_contract import PropertyRentContract 
+from contract.domain.enums import ( 
+                                   ContractStatus, 
+                                   PartyType, 
+                                   PRContractState, 
+                                   PRContractStep)
