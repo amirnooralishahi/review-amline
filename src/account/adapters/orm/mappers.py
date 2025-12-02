f10388a75c36d372529470b3c35404f7eb252aa3
unknown <amirnor2344@gmail.com>
2025-12-01 23:28:38 -0800

    start write code data_models for cotract service

---
+++ src/account/adapters/orm/mappers.py
@@ -0,0 +1,13 @@
+from sqlalchemy.orm import relationship 
+
+from account.adapters.orm import data_models,read_only_models 
+from contract.adapters.orm import data_models , read_only__models 
+from contract.admin import entities 
+from core.database import SQLALCHEMY_READNONLY_REGISTRY,SQLACLHEMY_REGISTERY 
+from shared.adapters.orm.read_only_models import PropertyROM
+
+
+
+def start_mappers() : 
+    
+    pass 
