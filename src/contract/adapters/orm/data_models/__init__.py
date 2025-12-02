d6973bb1b500c9461cd580e82fd743800d60eb20
unknown <amirnor2344@gmail.com>
2025-12-02 01:06:55 -0800

    end write contract_party_data_model

--- src/contract/adapters/orm/data_models/__init__.py
+++ src/contract/adapters/orm/data_models/__init__.py
@@ -1,5 +1,7 @@
 from .base_contract_clauses_data_model import base_contract_clauses
 from .cheque_data_model import cheques 
-from .contract_clause_data_model import contract_clauses 
+from .contract_clause_data_model import contract_clausess 
 from .contract_data_model import contracts 
+from .contract_party_data_model import contract_parties 
+
 
