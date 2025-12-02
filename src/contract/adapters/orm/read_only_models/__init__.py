89f94549ab231746ec9564d49d4713a53d90d56b
unknown <amirnor2344@gmail.com>
2025-12-02 03:18:56 -0800

    add __init__ for folder read_only_models

---
+++ src/contract/adapters/orm/read_only_models/__init__.py
@@ -0,0 +1,5 @@
+from .contract_clause_rom import ContractClausesROM,contract_clauses_rom 
+from .contract_party_rom import ContractPartyROM ,contract_parties_rom 
+# from .contract_payments_roms import  
+
+
