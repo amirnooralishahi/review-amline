a98e70c2a6dc481896bd1de98fb3f07423aa198b
unknown <amirnor2344@gmail.com>
2025-12-03 00:17:13 -0800

    start write contract_step_rom for orm read_only_model contract service

--- src/contract/adapters/orm/read_only_models/contract_rom.py
+++ src/contract/adapters/orm/read_only_models/contract_rom.py
@@ -3,3 +3,7 @@ from datetime import date as _date
 
 import sqlalchemy as sa
 from account.adapters.orm.read_only_models.bank_account_rom import BankAccountROM
+from contract.adapters.orm.read_only_models.contract_clause_rom import ConractClausesROM 
+from contract.adapters.orm.read_only_models.contract_party_rom import ContractPartyROM 
+from contract.adapters.orm.read_only_models.contract_payments_roms import ContractPaymentROM 
+from contract.adapters.orm.read_only_models.contract_step_rom import Contract
